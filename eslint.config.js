8e1f7fbe6ad717fb9b0cf5b2c580be87a6570d39
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-14 20:52:09 -0500

    Add test for site title
    Test currently failing (red phase)

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,27 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import {globalIgnores} from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommendedTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+            reactHooks.configs['recommended-latest'],
+            reactX.configs['recommended-typescript'],
+            reactDom.configs.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
     },
-  },
 ])
---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from './App';
+
+describe('App', () => {
+    it('should render the main title', () => {
+        render(<App />);
+
+        const mainTitle = screen.getByRole('heading', { name: /sleep debt tracker/i });
+
+        expect(mainTitle).toBeInTheDocument();
+    })
+})
