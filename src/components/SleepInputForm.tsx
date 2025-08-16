46ee3210b2b793f48e75bc21a68dd0c436375c1c
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-16 17:14:10 -0500

    Add inputs for hours, minutes, and button
    Test passes - Green Phase

--- src/components/SleepInputForm.tsx
+++ src/components/SleepInputForm.tsx
@@ -1,7 +1,17 @@
 function SleepInputForm() {
     return (
         <form aria-label={"Sleep Input Form"}>
-            {/* space for inputs */}
+            <div>
+                <label htmlFor="hours">Hours</label>
+                <input id="hours" type="number" />
+            </div>
+
+            <div>
+                <label htmlFor="minutes">Minutes</label>
+                <input id="minutes" type="number" />
+            </div>
+
+            <button type="submit">Record Sleep</button>
         </form>
     );
 }
