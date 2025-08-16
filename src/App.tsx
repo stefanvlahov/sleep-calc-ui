02bf02b8babf8d06219f19bd82b1012342ec6140
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-16 17:00:38 -0500

    Add input form
    Test passes - Green Phase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import SleepInputForm from "./components/SleepInputForm";
+
 function App() {
     return (
         <div>
             <h1>Sleep Debt Tracker</h1>
+            <SleepInputForm />
         </div>
     )
 }
---
+++ src/components/SleepInputForm.tsx
@@ -0,0 +1,9 @@
+function SleepInputForm() {
+    return (
+        <form aria-label={"Sleep Input Form"}>
+            {/* space for inputs */}
+        </form>
+    );
+}
+
+export default SleepInputForm;
